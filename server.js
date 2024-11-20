4d79a7caa22ee32cb3089058432a9c6dafd92e2a
Ricardo Ribeiro <ricardoribeirodematos@gmail.com>
2024-11-20 18:33:24 -0300

    Aula 3 - Finalizada

--- server.js
+++ server.js
@@ -1,65 +1,9 @@
 import express from "express";
-import conectarAoBanco from "./src/config/dbConfig.js";
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
-
-const posts = [
-  {
-    id: 1,
-    descricao: "Uma foto teste",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 2,
-    descricao: "Gato brincando com um novelo de lã",
-    imagem: "https://placekitten.com/400/200",
-  },
-  {
-    id: 3,
-    descricao: "Paisagem com um gato",
-    imagem: "https://placekitten.com/600/300",
-  },
-  {
-    id: 4,
-    descricao: "Gato dormindo em uma caixa",
-    imagem: "https://placekitten.com/800/400",
-  },
-  {
-    id: 5,
-    descricao: "Gato olhando pela janela",
-    imagem: "https://placekitten.com/1000/500",
-  },
-  {
-    id: 6,
-    descricao: "Gato comendo ração",
-    imagem: "https://placekitten.com/1200/600",
-  },
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
   console.log("Servidor escutando...");
-});
-
-async function getTodosPosts() {
-  const db = conexao.db("imersao-instabytes");
-  const colecao = db.collection("posts");
-  return colecao.find().toArray();
-}
-
-app.get("/posts", async (req, res) => {
-  const posts = await getTodosPosts();
-  res.status(200).json(posts);
-});
-
-function buscarPost(id) {
-  return posts.findIndex((post) => {
-    return post.id === Number(id);
-  });
-}
-
-app.get("/posts/:id", (req, res) => {
-  const index = buscarPost(req.params.id);
-  res.status(200).json(posts[index]);
 });
